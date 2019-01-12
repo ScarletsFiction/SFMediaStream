93e4b15a1484de17802249c6ae25537d1470a8d7
StefansArya <arya.cipta@yahoo.com>
2019-01-12 14:16:29 +0700

    Initialize audio context after any user interaction

--- src/a_init.js
+++ src/a_init.js
@@ -11,7 +11,7 @@
 
 // Initialize global data
 window.ScarletsMedia = {
-	audioContext: window.AudioContext ? new AudioContext() : false,
+	audioContext: false, // Created after user gesture
 
 	// Get Audio Node from HTML5's audio tag
 	getElementAudioNode:function(elem){
@@ -22,8 +22,43 @@ window.ScarletsMedia = {
 	// videoContext: window.VideoContext ? new VideoContext() : false,
 
 	// Still underdevelopment: https://github.com/bbc/VideoContext
-	getElementVideoNode:function(){
+	getElementVideoNode:function(elem){
 		elem.crossOrigin = 'anonymous';
 		return null;
 	}
-}
+};
+
+// Unlock mobile media security
+(function(){
+	var mobileMediaUnlock = function(e){
+		if(!window.AudioContext) return removeListener();
+		ScarletsMedia.audioContext = new AudioContext();
+
+		var emptyBuffer = ScarletsMedia.audioContext.createBuffer(1, 1, 22050);
+		var source = ScarletsMedia.audioContext.createBufferSource();
+		source.buffer = emptyBuffer;
+		source.connect(ScarletsMedia.audioContext.destination);
+
+		source.onended = function(){
+			source.disconnect(0);
+			source = emptyBuffer = null;
+
+			removeListener();
+		}
+
+		// Play the empty buffer.
+		if(!source.start) source.noteOn(0);
+		else source.start(0);
+		ScarletsMedia.audioContext.resume();
+	}
+
+	function removeListener(){
+		document.removeEventListener('touchstart', mobileMediaUnlock, true);
+		document.removeEventListener('touchend', mobileMediaUnlock, true);
+		document.removeEventListener('click', mobileMediaUnlock, true);
+	}
+
+	document.addEventListener('touchstart', mobileMediaUnlock, true);
+	document.addEventListener('touchend', mobileMediaUnlock, true);
+	document.addEventListener('click', mobileMediaUnlock, true);
+})();
